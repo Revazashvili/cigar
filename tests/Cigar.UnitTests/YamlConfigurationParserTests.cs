e1ae12ad43db6a86813d9a51f0b3a39890c2044d
revazashvili <levanrevazashvili@gmail.com>
2022-09-20 19:38:24 +0400

    add Cigar.UnitTests.csproj

---
+++ tests/Cigar.UnitTests/YamlConfigurationParserTests.cs
@@ -0,0 +1,25 @@
+using Cigar.Parsers;
+
+namespace Cigar.UnitTests;
+
+public class YamlConfigurationParserTests
+{
+    [Fact]
+    public async Task Reads_Valid_Yaml_File()
+    {
+        var configurationParser = new YamlConfigurationParser();
+        var configuration = await configurationParser.ParseAsync("valid.yaml");
+        Assert.NotNull(configuration);
+        Assert.NotNull(configuration.Execution);
+        Assert.NotEmpty(configuration.Execution.Steps);
+
+        Assert.Equal("http://localhost:8080", configuration.BaseUrl);
+    }
+    
+    [Fact]
+    public async Task Throws_Exception_On_Invalid_Yaml_File()
+    {
+        var configurationParser = new YamlConfigurationParser();
+        await Assert.ThrowsAnyAsync<Exception>(async () => await configurationParser.ParseAsync("invalid.yaml"));
+    }
+}
