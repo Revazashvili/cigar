7a361f22907e6ebf375dd8ac4aedacbfe55ec233
revazashvili <levanrevazashvili@gmail.com>
2022-09-24 11:27:35 +0400

    add file path reading from command file

--- src/Cigar/Program.cs
+++ src/Cigar/Program.cs
@@ -1,3 +1,44 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World!");
+using Cigar.Creators;
+using Cigar.Models;
+using Cigar.Parsers;
+using NBomber.CSharp;
+
+if (!args.Any())
+{
+    // TODO: print some message
+    Console.WriteLine("some logo");
+    return;
+}
+
+var filePath = Environment.GetCommandLineArgs()[1];
+if (!File.Exists(filePath))
+{
+    Console.WriteLine($"Can't find file on path: {filePath}");
+    return;
+}
+
+Console.WriteLine(Path.GetExtension(filePath));
+if (Path.GetExtension(filePath) != ".yaml")
+{
+    Console.WriteLine("please provide yaml file");
+    return;
+}
+
+var parser = new YamlConfigurationParser();
+Configuration? configuration = null;
+try
+{
+    configuration = await parser.ParseAsync(filePath);
+}
+catch (Exception)
+{
+    Console.WriteLine("Can't parse yaml file");
+}
+
+if (configuration is not null)
+{
+    var context = NBomberContextCreator.Create(Path.GetFileName(filePath), configuration);
+    context.Run();
+}
